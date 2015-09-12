700b062233a28b43c12d174cc6c76a4425b174a0
Michael Wong <michael.brian.wong@gmail.com>
2015-09-12 11:15:37 -0500

    Generate phone number and associate with person

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150912003032) do
+ActiveRecord::Schema.define(version: 20150912161416) do
 
   create_table "people", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "phone_numbers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
